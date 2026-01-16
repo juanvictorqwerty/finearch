26674461b51e133a1c1c465426911862e7a65e4e
MIKE Juan Victor <juanvictorqwerty@gmail.com>
2026-01-16 15:43:55 +0100

    Start back

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,9 @@
-import Login from "@/components/auth/login"
+import LoginUI from "@/components/auth/LoginComp";
 
-const LoginPage = () =>{
+const LoginPage=()=>{
     return(
-        <>
-            <div className=" flex items-center justify-center h-screen bg-white dark:bg-gray-900">
-                <Login/>
-            </div>
-        </>
+        <LoginUI/>
     )
-}
+};
 
 export default LoginPage;
---
+++ src/app/auth/signUp/page.tsx
@@ -0,0 +1,9 @@
+import SignUPUI from "@/components/auth/SignUpComp";
+
+const SignUpPage=()=>{
+    return(
+        <SignUPUI/>
+    )
+};
+
+export default SignUpPage;
